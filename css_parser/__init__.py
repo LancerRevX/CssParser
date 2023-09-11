afe4554a3eaf00520510b9d536144e038b37147b
nikitakirenkov <nikitakirenkov@gmail.com>
2023-09-11 16:59:03 +0700

    start new branch

--- css_parser/__init__.py
+++ css_parser/__init__.py
@@ -1,199 +1,29 @@
 import re
 from typing import Self
 import os
+from abc import ABC, abstractmethod
 
 import universal_parser
-from universal_parser import (
-    Element, BasicElement, RegexElement, ComplexElement, BlockElement, ListElement, ElementDefinition
-)
+
+class Element:
+    @property
+    @abstractmethod
+    def source_text(self):
+        pass
 
 
-class Space(RegexElement):
-    REGEX = r'\s+'
-
-
-class Comma(RegexElement):
-    REGEX = r','
-
-
-class Colon(RegexElement):
-    REGEX = r':'
-
-
-class Semicolon(RegexElement):
-    REGEX = r';'
-
-
-class PlainText(RegexElement):
-    REGEX = r'[\s\S]*'
-
-
-class Comment(BlockElement):
-    ELEMENT_DEFINITIONS = [
-        ElementDefinition(PlainText, required=False, single=True)
-    ]
-
-    START_STR = '/*'
-    END_STR = '*/'
-
-    def __repr__(self, depth=0) -> str:
-        return '\t' * depth + self.__class__.__name__ + ': /*' + self.content + '*/'
-
-
-SPACES_AND_COMMENTS = ElementDefinition([
-    ElementDefinition(Space, required=False, single=False),
-    ElementDefinition(Comment, required=False, single=False)
-], required=False, single=False)
-
-
-class SelectorItem(RegexElement):
-    REGEX = r'[][\n@ ^<>()\w:*="#_.-]*[]@[^<>().:\w#_-]+'
-
-
-class Selector(ListElement):
-    ELEMENT_CLASS = SelectorItem
-    DELIMETER = ','
-
-
-class Property(RegexElement):
-    REGEX = r'[-a-zA-Z0-9]+'
-
-
-
-
-class ValueText(RegexElement):
-    REGEX = r'[-\'\\\/\w.:!#_ ,%]+'
-
-
-class ValueTextParentheses(RegexElement):
-    REGEX = r'[-\'\\\/\w.:; ,%]+'
-
-
-class ParenthesesGroup(BlockElement):
-    START_STR = '('
-    END_STR = ')'
-
-    ELEMENT_DEFINITIONS = [
-        ElementDefinition(ValueTextParentheses, required=True, single=True),
-    ]
-
-
-class QuotedValue(BlockElement):
-    START_STR = '"'
-    END_STR = '"'
-
-    ELEMENT_DEFINITIONS = [
-        ElementDefinition(ValueText, required=False, single=True)
-    ]
-
-
-class Value(ComplexElement):
-    ELEMENT_DEFINITIONS = [
-        ElementDefinition([
-            ElementDefinition(ValueText, required=False, single=True),
-            ElementDefinition(QuotedValue, required=False, single=True),
-            ElementDefinition(ParenthesesGroup, required=False, single=True),
-            ElementDefinition(ValueText, required=False, single=True),
-        ], required=True, single=False)
-    ]
-
-    def __repr__(self, depth=0) -> str:
-        return '\t' * depth + self.__class__.__name__ + ': "' + self.source_text + '"'
-
-
-class Declaration(ComplexElement):
-    ELEMENT_DEFINITIONS = [
-        SPACES_AND_COMMENTS,
-        ElementDefinition(Property, required=True, single=True),
-        SPACES_AND_COMMENTS,
-        ElementDefinition(Colon, required=True, single=True),
-        SPACES_AND_COMMENTS,
-        ElementDefinition(Value, required=True, single=True),
-        SPACES_AND_COMMENTS
-    ]
+class Space(Element):
+    def __init__(self, source_text: str, pos: int | tuple[int, int]):
+        if type(pos) is int:
+            pos = (pos, pos)
+        match = re.match(r'^\s+', source_text[pos[0], pos[1]])
+        if match is None:
+            raise ParseError()
+        self.space_text = source_text
 
     @property
-    def value(self) -> str:
-        return self.get_elements_of_type(Value)[0].source_text
-    
-    @property
-    def property(self) -> str:
-        return self.get_elements_of_type(Property)[0].source_text
-
-
-
-class DeclarationList(ListElement):
-    ELEMENT_CLASS = Declaration
-    DELIMETER = ';'
-
-
-class DeclarationBlock(BlockElement):
-    START_STR = '{'
-    END_STR = '}'
-
-    ELEMENT_DEFINITIONS = [
-        SPACES_AND_COMMENTS,
-        ElementDefinition(DeclarationList, required=False, single=True),
-        SPACES_AND_COMMENTS
-    ]
-
-    @property
-    def vars(self):
-        vars = []
-        for element in self.elements:
-            if isinstance(element, Declaration) and element.is_var():
-                vars.append(element)
-        return vars
-
-
-class RuleSet(ComplexElement):
-    ELEMENT_DEFINITIONS = [
-        SPACES_AND_COMMENTS,
-        ElementDefinition(Selector, required=True, single=True),
-        SPACES_AND_COMMENTS,
-        ElementDefinition(DeclarationBlock, required=True, single=True),
-        SPACES_AND_COMMENTS,
-    ]
-
-    @property
-    def declarations(self) -> list[Declaration]:
-        return self.get_elements_of_type(Declaration, recursive=True)
-
-    @property
-    def properties(self):
-        return self.get_elements_of_type(Property, recursive=True)
-    
-    @property
-    def selector(self):
-        return self.get_elements_of_type(SelectorItem, recursive=True)
-
-
-class AtRuleSelector(RegexElement):
-    REGEX = r'@[][\n ^<>()\w:*="#_.-]*[]@[^<>().:\w#_-]+'
-
-
-class AtRuleBlock(BlockElement):
-    START_STR = '{'
-    END_STR = '}'
-
-    ELEMENT_DEFINITIONS = [
-        ElementDefinition([
-            ElementDefinition(RuleSet, required=False, single=False),
-            SPACES_AND_COMMENTS,
-            ElementDefinition(DeclarationList, required=False, single=False),
-            SPACES_AND_COMMENTS,
-        ], required=False, single=False)
-    ]
-
-
-class AtRule(ComplexElement):
-    ELEMENT_DEFINITIONS = [
-        SPACES_AND_COMMENTS,
-        ElementDefinition(AtRuleSelector, required=True, single=True),
-        SPACES_AND_COMMENTS,
-        ElementDefinition(AtRuleBlock, required=True, single=True),
-        SPACES_AND_COMMENTS
-    ]
+    def source_text(self):
+        return self.space_text
 
 
 class File(universal_parser.File):
@@ -204,6 +34,21 @@ class File(universal_parser.File):
             ElementDefinition(RuleSet, required=False, single=False)
         ], required=False, single=False),
     ]
+
+    @classmethod
+    def parse(cls, source_text: str) -> Self | None:
+        elements = []
+        for i in range(len(source_text)):
+            if re.match(r'^\s', source_text[i:]):
+                elements.append(Space.parse(source_text, i))
+            elif source_text[i:].startswith('/*'):
+                elements.append(Comment.parse(source_text, i))
+            elif source_text[i:].startswith('@'):
+                elements.append(AtRule.parse(source_text, i))
+            elif re.match(r'^[\w#.]', source_text[i:]):
+                elements.append(RuleSet.parse(source_text, i))
+            else:
+                raise ParseError()
 
     @property
     def comments(self):
